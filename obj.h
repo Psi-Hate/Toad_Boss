7dbcdd4d60142056f22b1cf50fcb263919bb3481
Psi-Hate <Psi-Hate@users.noreply.github.com>
2019-08-27 22:48:17 -0500

    Initial commit

---
+++ obj.h
@@ -0,0 +1,15 @@
+
+/* Display Lists * * * */
+     #define   DL_TOAD     0x060027B8
+
+/* Collisions * * * */
+
+/* Hierarchical Display Lists * * * */
+
+/* Animations * * * */
+     #define   ANIM_TOADACTION     0x06003178
+
+/* Hierarchies (Skeletons) * * * */
+     #define   SKL_DEFAULT     0x06003270
+
+/* Base Offset: 0x06000000 */
